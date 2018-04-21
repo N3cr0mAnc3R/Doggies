9547d9202e52cdc98e9b1eb19ce36eff8a4410c6
N3cr0mAnc3R <paul-profi@mail.ru>
2018-04-21 08:18:36 +0300

    Добавьте файлы проекта.

---
+++ Doggies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Doggies.Startup))]
+namespace Doggies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
